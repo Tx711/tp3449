daee03ab04bce6c773f3ecaeb423790dad634780
Tarun Prasad <94122432+tp3449@users.noreply.github.com>
2022-08-30 17:32:00 +0530

    practice question
    heros=['spider man','thor','hulk','iron man','captain America']
    1. Length of the list
    2. Add 'black panther' at the end of this list
    3. You realize that you need to add 'black panther' after 'hulk',
       so remove it from the list first and then add it after 'hulk'
    4. Now you don't like thor and hulk because they get angry easily :)
       So you want to remove thor and hulk from list and replace them with doctor strange (because he is cool).
       Do that with one line of code.
    5. Sort the heros list in alphabetical order (Hint. Use dir() functions to list down all functions available in list)

---
+++ practice code 2.py
@@ -0,0 +1,19 @@
+heros = ['spider man','thor','hulk','iron man','captain america']
+#1
+print(len(heros))
+#2
+heros.append('black panther')
+#3
+heros.remove('black panther')
+heros.insert(3,'black panther')
+print(heros)
+#4
+heros[1:3]=['Dr.Strange']
+print(heros)
+#5
+heros.sort()
+print(heros)
+
+
+
+
